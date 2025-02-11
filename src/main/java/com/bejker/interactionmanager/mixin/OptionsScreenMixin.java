78517b35a8d9ccc3e76647857c81d48c5d9b3730
bejker123 <1piotrkucharski@gmail.com>
2025-02-11 17:45:53 +0100

    fix: 'Interactions' button positioning

--- src/main/java/com/bejker/interactionmanager/mixin/OptionsScreenMixin.java
+++ src/main/java/com/bejker/interactionmanager/mixin/OptionsScreenMixin.java
@@ -3,6 +3,7 @@ package com.bejker.interactionmanager.mixin;
 import com.bejker.interactionmanager.gui.GuiCallbacks;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.GridWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +18,7 @@ public class OptionsScreenMixin extends Screen {
         super(title);
     }
 
-   @Inject(method = "init",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget;forEachChild(Ljava/util/function/Consumer;)V"),locals = LocalCapture.CAPTURE_FAILSOFT)
+   @Inject(method = "init",at= @At(value = "INVOKE",ordinal = 11,shift = At.Shift.AFTER, target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;"),locals = LocalCapture.CAPTURE_FAILSOFT)
     protected void onInit(CallbackInfo ci,GridWidget gridWidget,GridWidget.Adder adder) {
         if(gridWidget == null){
             return;
